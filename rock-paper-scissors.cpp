385843c498481c2165f54f319d7aed5445a6bcef
RicoLViviers <ricoleonardv@gmail.com>
2026-09-18 12:58:28 +0200

    implement rock paper scissor game loop and logic

--- rock-paper-scissors.cpp
+++ rock-paper-scissors.cpp
@@ -1,12 +1,55 @@
-﻿// rock-paper-scissors.cpp : Defines the entry point for the application.
-//
-
-#include "rock-paper-scissors.h"
-
-using namespace std;
+﻿#include "rock-paper-scissors.h"
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	std::srand(std::time(nullptr));
+	std::string input;
+	
+	while (true)
+	{
+
+		int computer = rand() % 3 + 1;   // 1=r, 2=p, 3=s
+
+
+		std::cout << "r/p/s: ";
+		std::cin >> input;
+
+		if (input == "r" && computer == 1) {
+			std::cout << "Draw: computer played rock" << std::endl;
+		}
+		else if (input == "r" && computer == 2) {
+			std::cout << "You lose: computer played paper" << std::endl;
+		}
+		else if (input == "r" && computer == 3) {
+			std::cout << "You win: computer played scissor" << std::endl;
+		}
+
+		if (input == "p" && computer == 1) {
+			std::cout << "You win: computer played rock" << std::endl;
+		}
+		else if (input == "p" && computer == 2) {
+			std::cout << "Draw: computer played paper" << std::endl;
+		}
+		else if (input == "p" && computer == 3) {
+			std::cout << "You lose: computer played scissor" << std::endl;
+		}
+
+		if (input == "s" && computer == 1) {
+			std::cout << "You lose: computer played rock" << std::endl;
+		}
+		else if (input == "s" && computer == 2) {
+			std::cout << "You win: computer played paper" << std::endl;
+		}
+		else if (input == "s" && computer == 3) {
+			std::cout << "Draw: computer played scissor" << std::endl;
+		}
+
+		if (input == "q") {
+			break;
+		}
+	}
+
 	return 0;
 }
