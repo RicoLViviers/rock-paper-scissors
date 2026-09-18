bc2c1a2570ab5bd434fdbb30afd5e2a7674b4808
RicoLViviers <ricoleonardv@gmail.com>
2026-09-18 12:45:17 +0200

    project initialization

---
+++ rock-paper-scissors.cpp
@@ -0,0 +1,12 @@
+﻿// rock-paper-scissors.cpp : Defines the entry point for the application.
+//
+
+#include "rock-paper-scissors.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ rock-paper-scissors.h
@@ -0,0 +1,8 @@
+﻿// rock-paper-scissors.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
